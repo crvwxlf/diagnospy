345c337224e23632850959a61860e09d521d0085
Kwalix <lyk4n@protonmail.com>
2020-05-16 00:00:27 +0200

    error handling added and network info feature added

--- diagnospy.py
+++ diagnospy.py
@@ -5,6 +5,7 @@ __version__ = " 1.0"
 
 import sys
 import os
+import socket
 from pyfiglet import figlet_format
 from termcolor2 import c
 
@@ -18,30 +19,39 @@ def menu():
 
 def main():
     print(c('\n1: Hardware Information').yellow)
+    print(c('\n2: Network info').yellow)
     print(c('\n0: Quit').yellow)
 
-    opt = input(c('\nChoose mode >>> ').cyan)
-    opt = int(opt)
+    try:
+        opt = input(c('\nChoose mode >>> ').cyan)
+        opt = int(opt)
+    except (NameError, SyntaxError):
+        print(c('\nBad option').red)
+        main()
+
     if opt == 0:
         print(c('\nExiting program...').red)
         sys.exit(0)
     elif opt == 1:
         print(c('\nHostname : ').yellow + os.popen('uname -n').read())
-        print(c('Platform support : ').yellow + os.popen("uname -m").read())
+        print(c('\nPlatform architecture : ').yellow + os.popen("uname -m").read())
+        print(c('\nOS Name : ').yellow + os.popen('uname -s').read())
+        print(c('\nOS Version : ').yellow + os.popen('uname -v').read())
+    elif opt == 2:
+        print(c('\nInternal IP : ').yellow + os.popen('hostname -I').read())
+        print(c('\nhostname IP :').yellow + socket.gethostbyname(socket.gethostname()))
+        print(c('\nExternal IP : ').yellow + c('Coming soon...').red.blink)
     else:
-        print('You are not choose option')
+        print(c('\nYou did not choose an option :(').red)
         main()
 
     while opt != 0:
         main()
 
 
+menu()
 try:
-    menu()
     main()
 except KeyboardInterrupt:
-    print(c('\nExiting program...').red)
+    print(c('\nExiting program...\n').red)
     sys.exit(0)
-except (SyntaxError, NameError):
-    print(c('\nBad option').red)
-    main()
