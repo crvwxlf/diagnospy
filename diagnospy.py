f89bda2e0232b544b89f8e93655433cd15a72f39
Kwalix <lyk4n@protonmail.com>
2020-05-15 02:15:31 +0200

    Added First option in  diagnospy

--- diagnospy.py
+++ diagnospy.py
@@ -5,9 +5,33 @@ __version__ = " In developement..."
 
 import sys
 import os
+import time
 from pyfiglet import figlet_format
 from termcolor2 import c
+
 
 print(c(figlet_format("Diagnospy")).red)
 print(c('Author :').white.on_blue + c(__author__).white)
 print(c('Versions:').white.on_red + __version__)
+
+
+def choice():
+    print(c('\n1: Hardware Information').yellow)
+    print(c('\n00: Quit').yellow)
+    opt = input(c('\nChoose mode >>> ').cyan)
+    opt = int(opt)
+    if opt == 00:
+        print(c('Exiting program...').red)
+        sys.exit(0)
+    elif opt == 1:
+        print(c('\nHostname : ').yellow + os.popen('uname -n').read())
+        print(c('Platform support : ').yellow + os.popen("uname -m").read())
+    while opt != 00:
+        os.system('clear')
+        choice()
+
+try:
+    choice()
+except (KeyboardInterrupt, SystemExit) as exit:
+    print('\nKeyboardInterrupt Detected, press Ctrl+C one more time for exit now')
+    time.sleep(2)
