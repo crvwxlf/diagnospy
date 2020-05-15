4368fffed5b1573864788ef5c6075e2631ceffd5
Kwalix <lyk4n@protonmail.com>
2020-05-15 02:57:41 +0200

    Patched first error

--- diagnospy.py
+++ diagnospy.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python
 # coding: utf-8
 __author__ = " Kwalix"
-__version__ = " In developement..."
+__version__ = " 1.0"
 
 import sys
 import os
-import time
 from pyfiglet import figlet_format
 from termcolor2 import c
 
 
-print(c(figlet_format("Diagnospy")).red)
-print(c('Author :').white.on_blue + c(__author__).white)
-print(c('Versions:').white.on_red + __version__)
+def menu():
+    os.system('clear')
+    print(c(figlet_format("Diagnospy")).red)
+    print(c('Author :').white.on_blue + c(__author__).white)
+    print(c('Versions:').white.on_red + __version__)
 
 
-def choice():
+def main():
     print(c('\n1: Hardware Information').yellow)
-    print(c('\n00: Quit').yellow)
+    print(c('\n0: Quit').yellow)
+
     opt = input(c('\nChoose mode >>> ').cyan)
     opt = int(opt)
-    if opt == 00:
-        print(c('Exiting program...').red)
+    if opt == 0:
+        print(c('\nExiting program...').red)
         sys.exit(0)
     elif opt == 1:
         print(c('\nHostname : ').yellow + os.popen('uname -n').read())
         print(c('Platform support : ').yellow + os.popen("uname -m").read())
-    while opt != 00:
-        os.system('clear')
-        choice()
+    else:
+        print('You are not choose option')
+        main()
+
+    while opt != 0:
+        main()
+
 
 try:
-    choice()
-except (KeyboardInterrupt, SystemExit) as exit:
-    print('\nKeyboardInterrupt Detected, press Ctrl+C one more time for exit now')
-    time.sleep(2)
+    menu()
+    main()
+except KeyboardInterrupt:
+    print(c('\nExiting program...').red)
+    sys.exit(0)
+except (SyntaxError, NameError):
+    print(c('\nBad option').red)
+    main()
