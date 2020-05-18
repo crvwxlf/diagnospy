49405111f6292cd8245c0da138eaab1c2593eec0
Kwalix <lyk4n@protonmail.com>
2020-05-18 16:34:04 +0200

    Reformating file added PEP 8 rules and Changed save_report function

--- diagnospy.py
+++ diagnospy.py
@@ -14,78 +14,82 @@ from termcolor2 import c
 # Global vars
 
 global host_n, arch, os_name, os_ver, iip, hip, eip, cwd
-host_n = os.popen('uname -n').read()
+host_n = os.popen("uname -n").read()
 arch = os.popen("uname -m").read()
-os_name = os.popen('uname -s').read()
-os_ver = os.popen('uname -v').read()
-iip = os.popen('hostname -I').read()
+os_name = os.popen("uname -s").read()
+os_ver = os.popen("uname -v").read()
+iip = os.popen("hostname -I").read()
 hip = socket.gethostbyname(socket.gethostname())
 cwd = os.getcwd()
 
 
 def menu():
-    os.system('clear')
+    os.system("clear")
     print(c(figlet_format("Diagnospy")).red)
-    print(c('Author :').white.on_blue + c(__author__).white)
-    print(c('Versions:').white.on_red + __version__)
+    print(c("Author :").white.on_blue + c(__author__).white)
+    print(c("Versions:").white.on_red + __version__)
 
 
 def main():
     # Choice menu
-    print(c('\n1: Hardware Information').yellow)
-    print(c('\n2: Network info').yellow)
-    print(c('\n3: Create and save diagnostic report in a txt file').yellow)
-    print(c('\n99: Quit').yellow)
+    print(c("\n1: Hardware Information").yellow)
+    print(c("\n2: Network info").yellow)
+    print(c("\n3: Create and save diagnostic report in a txt file").yellow)
+    print(c("\n99: Quit").yellow)
 
     try:
-        opt = input(c('\nChoose mode >>> ').cyan)
+        opt = input(c("\nChoose mode >>> ").cyan)
         opt = int(opt)
     except (NameError, SyntaxError):
-        print(c('\nBad option').red)
+        print(c("\nBad option").red)
         time.sleep(0.5)
-        os.system('clear')
+        os.system("clear")
         menu()
         main()
 
     if opt == 99:
-        print(c('\nExiting program...').red)
+        print(c("\nExiting program...").green)
         sys.exit(0)
     elif opt == 1:
-        print(c('\nHostname : ').yellow + host_n)
-        print(c('\nPlatform architecture : ').yellow + arch)
-        print(c('\nOS Name : ').yellow + os_name)
-        print(c('\nOS Version : ').yellow + os_ver)
+        print(c("\nHostname : ").yellow + host_n)
+        print(c("\nPlatform architecture : ").yellow + arch)
+        print(c("\nOS Name : ").yellow + os_name)
+        print(c("\nOS Version : ").yellow + os_ver)
     elif opt == 2:
-        print(c('\nInternal IP : ').yellow + iip)
-        print(c('\nhostname IP :').yellow + hip)
-        print(c('\nExternal IP : ').yellow + c('Coming soon...').red.blink)
+        print(c("\nInternal IP : ").yellow + iip)
+        print(c("\nhostname IP :").yellow + hip)
+        print(c("\nExternal IP : ").yellow + c("Coming soon...").red.blink)
     elif opt == 3:
         save_report()
         try:
-            askquit = input(c('\nEnter 1 for exit\nEnter 0 or Enter for continue\nExit Diagnospy ? : ').cyan)
+            askquit = input(
+                c(
+                    "\nEnter 1 for exit\nEnter 0 or Enter for continue\nExit Diagnospy ? : "
+                ).cyan
+            )
             askquit = int(askquit)
         except (SyntaxError, ValueError):
-            print(c('\nReturning to main menu').green)
+            print(c("\nReturning to main menu").green)
             time.sleep(0.5)
-            os.system('clear')
+            os.system("clear")
             menu()
             main()
         except NameError:
             askquit
 
         if askquit == 1:
-            print(c('\nGood bye').green)
+            print(c("\nGood bye").green)
             sys.exit(0)
         else:
-            print(c('\nReturning to main menu').green)
+            print(c("\nReturning to main menu").green)
             time.sleep(0.5)
-            os.system('clear')
+            os.system("clear")
             menu()
             main()
     else:
-        print(c('\nYou did not choose an option :(').red)
+        print(c("\nYou did not choose an option :(").red)
         time.sleep(0.5)
-        os.system('clear')
+        os.system("clear")
         menu()
         main()
 
@@ -94,14 +98,28 @@ def main():
 
 
 def save_report():
-    file_name = datetime.datetime.date(datetime.datetime.now())
-    file_name = str(file_name)
-    file_name = file_name + '.txt'
-    file = open(file_name, 'w+')
-    file.write(
-        'Hostname : ' + host_n + '\nArch : ' + arch + '\nOS Name : ' + os_name + '\nOS Version : ' + os_ver + '\nInternal IP : ' + iip + '\nHostname IP ' + hip + '\nExternal IP : N\A')
-    file.close()
-    print(c('\nyour file as been saved in : ').yellow + cwd + '/' + file_name)
+    file_name = datetime.datetime.date(datetime.datetime.now())  # Generate actual date
+    file_name = str(file_name)  # Converting date to string format
+    file_name = file_name + ".txt"  # add .txt suffix to file name
+    with open(file_name, "w+") as file:  # Creating file and write all data and close it
+        file.write(
+            "Hostname : "
+            + host_n
+            + "\nArch : "
+            + arch
+            + "\nOS Name : "
+            + os_name
+            + "\nOS Version : "
+            + os_ver
+            + "\nInternal IP : "
+            + iip
+            + "\nHostname IP "
+            + hip
+            + "\nExternal IP : N\A"
+        )
+    # Returning path of report file
+    print(c("\nyour file as been saved in : ").yellow + cwd + "/" + file_name)
+    return 0
 
 
 # Launching menu and main function
@@ -109,5 +127,5 @@ menu()
 try:
     main()
 except KeyboardInterrupt:
-    print(c('\nExiting program...\n').red)
+    print(c("\nExiting program...\n").red)
     sys.exit(0)
